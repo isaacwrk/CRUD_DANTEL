202561f6ea150d1d39b5f9f83f52b5feb72ce16c
Isaac <isaacsouzawrk@gmail.com>
2020-12-01 11:35:46 -0300

    update

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ use App\Http\Controllers\ProjectController;
 
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('projects');
 });
 
 Route::resource('projects', ProjectController::class);
